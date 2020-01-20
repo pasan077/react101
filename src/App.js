9da026b1a1becd3864e31ba971957ad47fe20b39
Pasan Jayathilaka <pasan4all@gmail.com>
2020-01-20 12:07:19 +0530

    20-01-2020 1207hrs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import "./App.css";
 import Todos from "./components/ToDos";
 
 function App() {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  };
   return (
     <div className="App">
       <h1>app</h1>
